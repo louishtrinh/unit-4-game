736b1b74ea58a935977e7753a6d154d213564fa6
LT <louis.h.trinh@gmail.com>
2018-10-13 17:40:30 -0700

    working on atk button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,30 @@
 warriorOfLight = {
-  heroBaseAtk: 0,
+  /* Hero Info */
+  lightChampion: [],
   heroAtk: 0,
-  elHeroAtk: [],
-  monsterMaxHP:0,
-  monsterHP:0,
-  elMonsterHP:[],
-  currentXP: [],
+  heroHP: 0,
+  heroMag: 0,
+  heroXP: 0,
+  heroLV: 1,
+  heroAtkEl: [],
+  heroHPel: [],
+  heroMagEl: [],
+  heroXPel: [],
+  heroLVel: [],
+  heroToBanish: [],
+  /* Hero Info */
+
+  /* Monster Info */
+  darkMinion: [],
+  monsterAtk: 0,
+  monsterMag: 0,
+  monsterHP: 0,
+  monsterHPel: [],
+  cardToBanish: [],
+  /* Monster Info */
+
+  score: 0,
   levelUpXP: [5, 10, 20, 40, 80, 160, 320, 640, 1280],
-  currentLV: [],
-  currentHP: [],
-  maxHP: [],
 
   hero: {
     sora: {
@@ -49,16 +64,26 @@ warriorOfLight = {
         baseAtk: 5,
         baseHP: 100
       }
+    },
+    aqua: {
+      stat: {
+        baseAtk: 20,
+        baseHP: 380
+      }
+    },
+    xaldin: {
+      stat: {
+        baseAtk: 60,
+        baseHP: 500
+      }
     }
   },
 
   isGuarding: false,
+  firstStart: true,
   heroSelected: false,
   monsterSelected: false,
   inCombat: false,
-  inPeace: true,
-  gameStart: false,
-  gameFinish: false,
 
   /* Hide De-select button */
   initialize: function() {
@@ -66,9 +91,78 @@ warriorOfLight = {
     $("#deSelectDonald").hide();
     $("#deSelectRiku").hide();
     $("#deSelectPhantom").hide();
-  }
+    $("#deSelectAqua").hide();
+    $("#deSelectXaldin").hide();
+  },
   /* Hide De-select button */
+
+  getHeroInfo: function() {
+    this.heroAtk = this.lightChampion.stat.baseAtk;
+    this.heroMag = this.lightChampion.stat.baseMag;
+    this.heroHP = this.lightChampion.stat.baseHP;
+    this.heroXP = this.lightChampion.stat.baseXP;
+    this.heroLV = this.lightChampion.stat.baseLV;
+  },
+
+  getMonsterInfo: function() {
+    this.monsterHP = this.darkMinion.stat.baseHP;
+    this.monsterAtk = this.darkMinion.stat.baseAtk;
+  },
+
+  heroAttack: function() {
+    this.monsterHP = this.monsterHP - this.heroAtk;
+    $("#logs").prepend(
+      "<div>" +
+        $("#logs div").length +
+        ". " +
+        "Hero hits for " +
+        this.heroAtk +
+        " damage" +
+        "</div>"
+    );
+    this.heroLV++;
+    $("#logs").prepend(
+      "<div>" +
+        $("#logs div").length +
+        ". " +
+        "Level Up! ATK increase by " +
+        this.lightChampion.stat.baseAtk +
+        "." +
+        "</div>"
+    );
+    this.heroAtk = this.heroAtk + this.lightChampion.stat.baseAtk;
+
+    this.heroAtkEl.text(this.heroAtk);
+    this.heroLVel.text(this.heroLV);
+    this.monsterHPel.text(this.monsterHP);
+  },
+
+  monsterCounter: function () {
+    this.heroHP = this.heroHP - this.monsterAtk;
+    this.heroHPel.text(this.heroHP);
+    $("#logs").prepend(
+      "<div>" +
+        $("#logs div").length +
+        ". " +
+        "Monster counters for " +
+        this.darkMinion.stat.baseAtk +
+        " damage" +
+        "</div>"
+    );
+  },
+
+  monsterBanish: function () {
+    this.inCombat=false;
+    this.score++;
+    this.cardToBanish.remove();
+    $(".monsterSelectBtn").show();
+    this.initialize();
+    $("#monsterCard").appendTo($("#fightArena"));
+  }
 };
+
+
+/* $("#logs").prepend("<div>" +$("#logs div").length +". " +"Comment goes here" + "</div>"); */
 
 $(document).ready(function() {
   warriorOfLight.initialize();
@@ -77,8 +171,16 @@ $(document).ready(function() {
   $("#selectSora").on("click", function() {
     if (!warriorOfLight.heroSelected) {
       warriorOfLight.heroSelected = true;
-      warriorOfLight.elHeroAtk = $("#soraCard #attack");
-      warriorOfLight.heroBaseAtk = warriorOfLight.hero.sora.stat.baseAtk;
+
+      /* Remember hero pick to obj */
+      warriorOfLight.lightChampion = warriorOfLight.hero.sora;
+
+      /* Latch to page */
+      warriorOfLight.heroToBanish = $("#soraCard");
+      warriorOfLight.heroLVel = $("#soraCard #level");
+      warriorOfLight.heroAtkEl = $("#soraCard #attack");
+      warriorOfLight.heroHPel = $("#soraCard #hp");
+
       $(this).hide(0);
       $("#deSelectSora").show(0);
       $("#soraCard").prependTo($("#fightArena"));
@@ -88,6 +190,16 @@ $(document).ready(function() {
   $("#selectDonald").on("click", function() {
     if (!warriorOfLight.heroSelected) {
       warriorOfLight.heroSelected = true;
+
+      /* Remember hero pick to obj */
+      warriorOfLight.lightChampion = warriorOfLight.hero.donald;
+
+      /* Latch to page */
+      warriorOfLight.heroToBanish = $("#donaldCard");
+      warriorOfLight.heroLVel = $("#donaldCard #level");
+      warriorOfLight.heroAtkEl = $("#donaldCard #attack");
+      warriorOfLight.heroHPel = $("#donaldCard #hp");
+
       $(this).hide(0);
       $("#deSelectDonald").show(0);
       $("#donaldCard").prependTo($("#fightArena"));
@@ -97,6 +209,16 @@ $(document).ready(function() {
   $("#selectRiku").on("click", function() {
     if (!warriorOfLight.heroSelected) {
       warriorOfLight.heroSelected = true;
+
+      /* Remember hero pick to obj */
+      warriorOfLight.lightChampion = warriorOfLight.hero.riku;
+
+      /* Latch to page */
+      warriorOfLight.heroToBanish = $("#rikuCard");
+      warriorOfLight.heroLVel = $("#rikuCard #level");
+      warriorOfLight.heroAtkEl = $("#rikuCard #attack");
+      warriorOfLight.heroHPel = $("#rikuCard #hp");
+
       $(this).hide(0);
       $("#deSelectRiku").show(0);
       $("#rikuCard").prependTo($("#fightArena"));
@@ -124,7 +246,6 @@ $(document).ready(function() {
       $("#heroCard").appendTo($("#fightArena"));
     }
   });
-
   $("#deSelectRiku").on("click", function() {
     if (warriorOfLight.heroSelected) {
       warriorOfLight.heroSelected = false;
@@ -141,12 +262,43 @@ $(document).ready(function() {
     if (!warriorOfLight.monsterSelected) {
       warriorOfLight.monsterSelected = true;
 
-      warriorOfLight.elMonsterHP = $("#phantomCard #hp");
-      warriorOfLight.monsterHP = warriorOfLight.monster.phantom.stat.baseHP;
+      warriorOfLight.cardToBanish = $("#phantomCard");
+      warriorOfLight.monsterHPel = $("#phantomCard #hp");
+      warriorOfLight.darkMinion = warriorOfLight.monster.phantom;
 
       $(this).hide(0);
       $("#deSelectPhantom").show(0);
       $("#phantomCard").appendTo($("#fightArena"));
+      $("#monsterCard").appendTo($("#monsterList"));
+    }
+  });
+
+  $("#selectAqua").on("click", function() {
+    if (!warriorOfLight.monsterSelected) {
+      warriorOfLight.monsterSelected = true;
+
+      warriorOfLight.cardToBanish = $("#aquaCard");
+      warriorOfLight.monsterHPel = $("#aquaCard #hp");
+      warriorOfLight.darkMinion = warriorOfLight.monster.aqua;
+
+      $(this).hide(0);
+      $("#deSelectAqua").show(0);
+      $("#aquaCard").appendTo($("#fightArena"));
+      $("#monsterCard").appendTo($("#monsterList"));
+    }
+  });
+
+  $("#selectXaldin").on("click", function() {
+    if (!warriorOfLight.monsterSelected) {
+      warriorOfLight.monsterSelected = true;
+
+      warriorOfLight.cardToBanish = $("#xaldinCard");
+      warriorOfLight.monsterHPel = $("#xaldinCard #hp");
+      warriorOfLight.darkMinion = warriorOfLight.monster.xaldin;
+
+      $(this).hide(0);
+      $("#deSelectXaldin").show(0);
+      $("#xaldinCard").appendTo($("#fightArena"));
       $("#monsterCard").appendTo($("#monsterList"));
     }
   });
@@ -162,62 +314,165 @@ $(document).ready(function() {
       $("#monsterCard").appendTo($("#fightArena"));
     }
   });
+  $("#deSelectAqua").on("click", function() {
+    if (warriorOfLight.monsterSelected) {
+      warriorOfLight.monsterSelected = false;
+      $(this).hide(0);
+      $("#selectAqua").show(0);
+      $("#aquaCard").prependTo($("#monsterList"));
+      $("#monsterCard").appendTo($("#fightArena"));
+    }
+  });
+  $("#deSelectXaldin").on("click", function() {
+    if (warriorOfLight.monsterSelected) {
+      warriorOfLight.monsterSelected = false;
+      $(this).hide(0);
+      $("#selectXaldin").show(0);
+      $("#xaldinCard").prependTo($("#monsterList"));
+      $("#monsterCard").appendTo($("#fightArena"));
+    }
+  });
   /* De-select Monsters */
 
   /* One-time hero selection */
   $("#atkBtn").on("click", function() {
-    if (!warriorOfLight.gameStart) {
-      if (!warriorOfLight.heroSelected && !warriorOfLight.monsterSelected) {
-        $("#logs").prepend(
-          "<div>" +
-            $("#logs div").length +
-            ". " +
-            "Please select your Light Champion and pick a Monster to fight" +
-            "</div>"
-        );
-      } else if (!warriorOfLight.heroSelected) {
-        $("#logs").prepend(
-          "<div>" +
-            $("#logs div").length +
-            ". " +
-            "Please select your Light Champion" +
-            "</div>"
-        );
-      } else if (!warriorOfLight.monsterSelected) {
-        $("#logs").prepend(
-          "<div>" +
-            $("#logs div").length +
-            ". " +
-            "Please pick a Monster to fight" +
-            "</div>"
-        );
-      } else {
-        warriorOfLight.gameStart = true;
-        warriorOfLight.inCombat = true;
-        $(".heroSelectBtn").remove();
-        $(".monsterSelectBtn").hide();
-        $("#logs").prepend(
-          "<div>" +
-            $("#logs div").length +
-            ". " +
-            "The game has begun, have fun!" +
-            "</div>"
-        );
+    if (warriorOfLight.firstStart){
+      if (!warriorOfLight.gameStart) {
+        if (!warriorOfLight.heroSelected && !warriorOfLight.monsterSelected) {
+          $("#logs").prepend(
+            "<div>" +
+              $("#logs div").length +
+              ". " +
+              "Please select your Light Champion and pick a Monster to fight" +
+              "</div>"
+          );
+        } else if (!warriorOfLight.heroSelected) {
+          $("#logs").prepend(
+            "<div>" +
+              $("#logs div").length +
+              ". " +
+              "Please select your Light Champion" +
+              "</div>"
+          );
+        } else if (!warriorOfLight.monsterSelected) {
+          $("#logs").prepend(
+            "<div>" +
+              $("#logs div").length +
+              ". " +
+              "Please pick a Monster to fight" +
+              "</div>"
+          );
+        } else {
+          warriorOfLight.firstStart = false;
+          warriorOfLight.inCombat = true;
+          warriorOfLight.getHeroInfo();
+          warriorOfLight.getMonsterInfo();
+          $(".heroSelectBtn").remove();
+          $(".monsterSelectBtn").hide();
+          $("#logs").prepend(
+            "<div>" +
+              $("#logs div").length +
+              ". " +
+              "The game has begun, have fun!" +
+              "</div>"
+          );
+        }
       }
     }
+
   });
   /* One-time hero selection */
 
-  /* Combat */
   $("#atkBtn").on("click", function() {
-    if (warriorOfLight.gameStart && warriorOfLight.inCombat) {
-        warriorOfLight.heroAtk = warriorOfLight.heroAtk + warriorOfLight.heroBaseAtk;
-        warriorOfLight.monsterHP = warriorOfLight.monsterHP - warriorOfLight.heroAtk;
-        warriorOfLight.elHeroAtk.text(warriorOfLight.heroAtk);
-        warriorOfLight.elMonsterHP.text(warriorOfLight.monsterHP);
+    if (warriorOfLight.inCombat) {
+      warriorOfLight.heroAttack();
+    } if (warriorOfLight.monsterHP > 0) {
+      warriorOfLight.monsterCounter();
+    } else {
+      warriorOfLight.monsterBanish();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
   });
-  /* Combat */
 });
 
 /* $("#logs").prepend("<div>" + $("#logs div").length + ". " + "Please select your Light Champion and pick a Monster to fight" + "</div>"); */
+/*    if (this.monsterHP > 0) {
+      this.heroHP = this.heroHP - this.monsterAtk;
+      $("#logs").prepend(
+        "<div>" +
+          $("#logs div").length +
+          ". " +
+          "Monster counters for " +
+          this.darkMinion.stat.baseAtk +
+          " damage" +
+          "</div>"
+      );
+      if (this.heroHP <= 0) {
+        inCombat = false;
+        gameStart = false;
+        gameFinish = true;
+        this.heroToBanish.remove();
+        $("#logs").prepend(
+          "<div>" +
+            $("#logs div").length +
+            ". " +
+            "YOU DIED - Game Over!" +
+            "</div>"
+        );
+      } else {
+        this.heroLV++;
+        $("#logs").prepend(
+          "<div>" +
+            $("#logs div").length +
+            ". " +
+            "Level Up! ATK increase by " +
+            this.lightChampion.stat.baseAtk +
+            "." +
+            "</div>"
+        );
+      }
+    } else if (this.monsterHP <= 0) {
+      this.cardToBanish.remove();
+      this.inCombat = false;
+      this.score++;
+      $("#logs").prepend(
+        "<div>" +
+          $("#logs div").length +
+          ". " +
+          "Darkness has been banish." +
+          "</div>"
+      );
+      if (this.score == 3) {
+        this.gameFinish = true;
+        $("#logs").prepend(
+          "<div>" +
+            $("#logs div").length +
+            ". " +
+            "All monsters have been banished. Once again, the world is save" +
+            "</div>"
+        );
+      } else if (this.score < 3) {
+        $(".monsterSelectBtn").show();
+        this.monsterSelected = false;
+        $("#monsterCard").appendTo($("#fightArena"));
+      }
+    }
+
+    this.heroAtk = this.heroAtk + this.lightChampion.stat.baseAtk;
+    warriorOfLight.heroAtkEl.text(warriorOfLight.heroAtk);
+    warriorOfLight.heroHPel.text(warriorOfLight.heroHP);
+    warriorOfLight.heroLVel.text(warriorOfLight.heroLV);
+    warriorOfLight.monsterHPel.text(warriorOfLight.monsterHP);
+  } */
